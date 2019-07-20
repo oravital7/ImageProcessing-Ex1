f9db06bf2ed68adaa9b1b0fb95aacb59a2204cb2
oravital7 <oravital7@gmail.com>
2019-07-20 17:39:48 +0300

    Q3

--- Exercise1.py
+++ Exercise1.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 try:
     from cv2 import cv2 as cv
 except ImportError:
@@ -39,5 +40,64 @@ def transformYIQ2RGB(imYIQ:np.ndarray)->np.ndarray:
     return np.dot(imYIQ, rgb_from_yiq.T.copy())
 
 
-# imDisplay("/home/oravital7/Downloads/rgbImage.png",2)
-transformYIQ2RGB(imReadAndConvert("/home/oravital7/Downloads/rgbImage.png",2))
+
+def histogramEqualize(imOrig:np.ndarray)->(np.ndarray,np.ndarray,np.ndarray):
+    imOrig *= 255
+    imOrig = imOrig.astype(int)
+    img1 = imOrig
+
+    height = imOrig.shape[0]
+    width = imOrig.shape[1]
+    pixels = width * height
+
+    if(not isGrayScale(imOrig)):
+        img1 = transformRGB2YIQ(imOrig)[:,:,0]
+        img1 = img1.astype(int)
+
+
+    hist_origi = histogram(img1)
+    cum_hist = cumulative_histogram(hist_origi)
+
+    for i in np.arange(height):
+        for j in np.arange(width):
+            a = img1.item(i, j)
+            b = math.floor(cum_hist[a] * 255.0 / pixels)
+            img1.itemset((i, j), b)
+
+    hist_eq = histogram(img1)
+    return (imOrig, hist_origi,hist_eq)
+
+
+
+def histogram(img):
+    height = img.shape[0]
+    width = img.shape[1]
+
+    hist = np.zeros((256))
+
+    for i in np.arange(height):
+        for j in np.arange(width):
+            a = img.item(i, j)
+            hist[a] += 1
+
+    return hist
+
+
+def cumulative_histogram(hist):
+    cum_hist = hist.copy()
+
+    for i in np.arange(1, 256):
+        cum_hist[i] = cum_hist[i - 1] + cum_hist[i]
+
+    return cum_hist
+
+def isGrayScale(img):
+    if(len(img.shape) < 3): return True
+    else: return False
+
+# imDisplay("/home/oravital7/Downloads/rgbImage.png",1)
+# transformYIQ2RGB(imReadAndConvert("/home/oravital7/Downloads/rgbImage.png",2))
+x = imReadAndConvert("/home/oravital7/Downloads/rgbImage.png",1)
+y = histogramEqualize(x)
+print(y[2])
+# print(y)
